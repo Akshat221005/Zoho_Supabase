5f566df801d47041dabccb3d0a2d0951f188dad8
Akshat <akshatd23@iitk.ac.in>
2026-03-29 22:00:49 +0530

    Remove Supabase credentials from supabase_int.py

--- supabase_int.py
+++ supabase_int.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 
-SUPABASE_URL = "https://lhkkhhqkqmtbeeswdjuq.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa2toaHFrcW10YmVlc3dkanVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNTQxMjksImV4cCI6MjA4ODgzMDEyOX0.XpAwHZiAB-lTh1Hro2XP4VQTKyD0ofow-4jBYfaNrts"
+SUPABASE_URL = ""
+SUPABASE_KEY = "s"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
